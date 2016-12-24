b2fc338541fef33104ec53c9821e61af13b1ff45
Danny Guo <dannyguo91@gmail.com>
2016-12-24 13:46:07 -0500

    Use Docker instead of Vagrant for development

--- Vagrantfile
+++
@@ -1,5 +0,0 @@
-Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/trusty64"
-  config.vm.provision :shell, path: "bootstrap.sh"
-end
-
--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo Installing Rust...
-curl -sf https://static.rust-lang.org/rustup.sh | bash -s -- --revision=1.10.0 -y
-echo "cd /vagrant" > /home/vagrant/.bashrc
-
