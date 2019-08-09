6fedc57848da2735b89a777574211472d0ba9366
Zachary Pierce <zack@auxon.io>
2019-08-09 15:55:33 -0700

    Add generative test cases

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,4 +1,6 @@
 extern crate strsim;
+extern crate proptest;
+use proptest::prelude::*;
 
 use strsim::{hamming, levenshtein, normalized_levenshtein, osa_distance,damerau_levenshtein,
              normalized_damerau_levenshtein, jaro, jaro_winkler};
@@ -46,4 +48,65 @@ fn jaro_works() {
 fn jaro_winkler_works() {
     assert!((0.911 - jaro_winkler("cheeseburger", "cheese fries")).abs() <
             0.001);
+}
+
+proptest! {
+    #[test]
+    fn hamming_arbitrary_strings_work(a in ".*", b in ".*") {
+        match hamming(a.as_str(), b.as_str()) {
+            Ok(v) => {
+                assert!(v <= a.len().max(b.len()));
+            },
+            Err(_) => {
+                assert_ne!(a.chars().count(), b.chars().count());
+            }
+        }
+    }
+    #[test]
+    fn jaro_arbitrary_strings_work(a in ".*", b in ".*") {
+        let v = jaro(a.as_str(), b.as_str());
+        assert!(v >= 0.0 && v <= 1.0);
+        if v == 1.0 {
+            assert_eq!(a, b);
+        }
+    }
+    #[test]
+    fn jaro_winkler_arbitrary_strings_work(a in ".*", b in ".*") {
+        let v = jaro_winkler(a.as_str(), b.as_str());
+        assert!(v >= 0.0 && v <= 1.0);
+        if v == 1.0 {
+            assert_eq!(a, b);
+        }
+    }
+    #[test]
+    fn levenshtein_arbitrary_strings_work(a in ".*", b in ".*") {
+        let v = levenshtein(a.as_str(), b.as_str());
+        assert!(v <= a.chars().count().max(b.chars().count()));
+    }
+    #[test]
+    fn normalized_levenshtein_arbitrary_strings_work(a in ".*", b in ".*") {
+        let v = normalized_levenshtein(a.as_str(), b.as_str());
+        assert!(v >= 0.0 && v <= 1.0);
+        if v == 1.0 {
+            assert_eq!(a, b);
+        }
+    }
+    #[test]
+    fn osa_distance_arbitrary_strings_work(a in ".*", b in ".*") {
+        let v = osa_distance(a.as_str(), b.as_str());
+        assert!(v <= a.chars().count().max(b.chars().count()));
+    }
+    #[test]
+    fn damerau_levenshtein_arbitrary_strings_work(a in ".*", b in ".*") {
+        let v = damerau_levenshtein(a.as_str(), b.as_str());
+        assert!(v <= a.chars().count().max(b.chars().count()));
+    }
+    #[test]
+    fn normalized_damerau_levenshtein_arbitrary_strings_work(a in ".*", b in ".*") {
+        let v = normalized_damerau_levenshtein(a.as_str(), b.as_str());
+        assert!(v >= 0.0 && v <= 1.0);
+        if v == 1.0 {
+            assert_eq!(a, b);
+        }
+    }
 }
